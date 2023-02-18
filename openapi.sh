d75788e2ebf317b119b8efca798391927f627ee6
Shitomo <cadaeibf@outlook.com>
2023-02-18 23:04:42 +0900

    chore(#3) openapiのコード生成をワンライナーに

--- openapi.sh
+++ openapi.sh
@@ -2,8 +2,6 @@
 
 BRANCH_NAME=main
 
-curl https://raw.githubusercontent.com/morning-night-guild/platform/${BRANCH_NAME}/api/openapi.yaml > openapi.yaml
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/openapi.yaml -g typescript-fetch -o /local/openapi
-rm -r ./src/openapi
-mv openapi/ src/
-rm openapi.yaml
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
+    -i https://raw.githubusercontent.com/morning-night-guild/platform/${BRANCH_NAME}/api/openapi.yaml \
+    -g typescript-fetch -o /local/src/openapi
