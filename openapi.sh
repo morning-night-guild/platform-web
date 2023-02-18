03439106e9979820e6dc6c1f60705dd0a2149064
Shitomo <cadaeibf@outlook.com>
2023-02-18 22:36:40 +0900

    chore(#3) openapiの仕様書からフロントエンドのコードを生成する仕組みを実装

---
+++ openapi.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+BRANCH_NAME=main
+
+curl https://raw.githubusercontent.com/morning-night-guild/platform/${BRANCH_NAME}/api/openapi.yaml > openapi.yaml
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/openapi.yaml -g typescript-fetch -o /local/openapi
+rm -r src/openapi && mv openapi src/openapi
+rm openapi.yaml
